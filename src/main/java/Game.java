0bc6df3ecfe98e5ceda55fd4f4c9caf4aa7770d5
Bernardo Costa Moreira <bernardocostamoreira@Bernardos-MacBook-Pro.local>
2019-02-28 16:10:59 +0000

    End of ex7

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,4 +1,6 @@
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -9,6 +11,9 @@ import java.io.IOException;
 public class Game {
 
     Screen screen;
+    private int x = 10;
+    private int y = 10;
+    private Boolean runVar = true;
 
     Game() {
         try {
@@ -26,18 +31,54 @@ public class Game {
         }
     }
 
-        private void draw() throws IOException {
+     private void draw() throws IOException {
             screen.clear();
-            screen.setCharacter(10, 10, new TextCharacter('X'));
+            screen.setCharacter(x, y, new TextCharacter('X'));
             screen.refresh();
+
     }
-        public void run(){
-            try{
-                this.draw();
+
+    public void run() {
+
+            try {
+                while (runVar) {
+                    this.draw();
+                    KeyStroke key = screen.readInput();
+                    this.processKey(key);
+                }
+                screen.close();
 
             } catch (IOException e) {
                 e.printStackTrace();
             }
+
+    }
+    private void processKey(KeyStroke key) {
+        System.out.println(key);
+
+        if (key.getKeyType() == KeyType.ArrowUp) {
+            if (this.y > 0) {
+                this.y--;
+            }
+
+        } else if (key.getKeyType() == KeyType.ArrowDown) {
+            this.y++;
+        } else if (key.getKeyType() == KeyType.ArrowLeft) {
+            if (this.x > 0) {
+                this.x--;
+            }
+        } else if (key.getKeyType() == KeyType.ArrowRight) {
+            this.x++;
         }
+
+        if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q'){
+            runVar = false;
+        }
+
+        if (key.getKeyType() == KeyType.EOF ){
+            runVar = false;
+        }
+    }
+
 }
 
